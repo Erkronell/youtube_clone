75439c1137cf1155f6119ae57459c2a020049e6e
erkronell <cedielle@gmail.com>
2021-09-05 17:02:48 -0400

    add responsive mode and clean up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import Youtube from './service/youtube-fetch';
+import Youtube from './service/youtube';
 import axios from 'axios';
 
 const httpClient = axios.create({
